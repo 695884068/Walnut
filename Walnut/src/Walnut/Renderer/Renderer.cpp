8c938f10320cc6abf9ef8c2f42ad9864ebeb4c65
695884068 <695884068@qq.com>
2022-01-05 21:47:16 +0800

    add transforms

--- Walnut/src/Walnut/Renderer/Renderer.cpp
+++ Walnut/src/Walnut/Renderer/Renderer.cpp
@@ -1,6 +1,8 @@
 #include "wnpch.h"
 #include "Renderer.h"
 
+#include "Plateform/OpenGL/OpenGLShader.h"
+
 namespace Walnut {
 	
 	Renderer::SceneData* Renderer::m_SceneData = new Renderer::SceneData;
@@ -15,10 +17,11 @@ namespace Walnut {
 
 	}
 
-	void Renderer::Submit(const std::shared_ptr<Shader>& shader, const std::shared_ptr<VertexArray>& vertexArray)
+	void Renderer::Submit(const std::shared_ptr<Shader>& shader, const std::shared_ptr<VertexArray>& vertexArray, const glm::mat4& transform)
 	{
 		shader->Bind();
-		shader->UploadUniformMat4("u_ViewProjection", m_SceneData->ViewProjectionMatrix);
+		std::dynamic_pointer_cast<OpenGLShader>(shader)->UploadUniformMat4("u_ViewProjection", m_SceneData->ViewProjectionMatrix);
+		std::dynamic_pointer_cast<OpenGLShader>(shader)->UploadUniformMat4("u_Transform", transform);
 
 		vertexArray->Bind();
 		RenderCommand::DrawIndexed(vertexArray);
